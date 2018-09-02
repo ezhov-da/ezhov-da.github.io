ee620018f141a729af4dbc90fbad524cb62350ed
ezhov-da <ezhdenis@yandex.ru>
2018-09-02 23:38:18 +0300

    Работа с удаленным сервером

--- library.js
+++ library.js
@@ -1,7 +1,7 @@
-// var urlListBooks = 'https://prog-tools.ru:8445/library/rest/books';
-var urlListBooks = 'http://localhost:8080/library/rest/books';
-// var urlDownloadLinkBook = 'https://prog-tools.ru:8445/library/rest/books/book/link';
-var urlDownloadLinkBook = 'http://localhost:8080/library/rest/books/book/link';
+var urlListBooks = 'https://prog-tools.ru:8445/library/rest/books';
+// var urlListBooks = 'http://localhost:8080/library/rest/books';
+var urlDownloadLinkBook = 'https://prog-tools.ru:8445/library/rest/books/book/link';
+// var urlDownloadLinkBook = 'http://localhost:8080/library/rest/books/book/link';
 
 Ext.create('Ext.data.Store', {
     storeId: 'bookStore',
