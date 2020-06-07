7d33f6aea32111fea6ef8dc02a8ab5920ffc628e
ezhov_da <ezhdenis@yandex.ru>
2020-06-07 16:54:47 +0300

    feat: добавлены аватар и ссылки на профили Github и LinkedIn

--- ezhov-da/src/App.js
+++ ezhov-da/src/App.js
@@ -21,14 +21,33 @@ class App extends React.Component {
         <div className="container">
             <nav className="navbar" role="navigation" aria-label="main navigation">
               <div className="navbar-brand">
-                <a className="navbar-item" href="https://github.com/ezhov-da">
-                 ezhov-da
-                </a>
               </div>
             </nav>
 
-            <div className="tabs is-centered">
+            <div className="level">
+                <div className="level-item">
+                    <figure class="image is-128x128">
+                      <img class="is-rounded" src="/avatar.jpg"/>
+                    </figure>
+                </div>
+            </div>
 
+            <div className="level">
+                <div className="level-item">
+                    <span class="icon has-text-info">
+                        <a href="https://github.com/ezhov-da">
+                            <i class="fab fa-github" aria-hidden="true"/>
+                        </a>
+                    </span>
+                    <span class="icon has-text-info">
+                        <a href="https://www.linkedin.com/in/ezhovda">
+                            <i class="fab fa-linkedin" aria-hidden="true"/>
+                        </a>
+                    </span>
+                </div>
+            </div>
+
+            <div className="tabs is-centered">
               <ul>
                 <li><NavLink exact={true} activeStyle={{borderBottomColor: "#3273dc", color: "#3273dc"}} to="/gists">Мои Gist</NavLink></li>
                 <li><NavLink activeStyle={{borderBottomColor: "#3273dc", color: "#3273dc"}} to="/books">Книги</NavLink></li>
